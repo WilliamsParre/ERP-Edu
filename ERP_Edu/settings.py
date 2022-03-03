bda4d2805906d38b6eaecf646f8b7d35c12eff08
WilliamsParre <williamsparre@gmail.com>
2022-03-04 01:25:02 +0530

    Adds DB

--- ERP_Edu/settings.py
+++ ERP_Edu/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'ERP_Edu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'ERP_Edu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'erp',
+        'USER': 'root',
+        'PASSWORD': 'MySQL',
+        'HOST': 'localhost'
     }
 }
 
--- base/views.py
+++ base/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Hello World, from Home</h1>')
+    return render(request, 'base/home.html')
 
