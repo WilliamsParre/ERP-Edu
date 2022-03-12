1873ad4e5722bf27d1bf519943c2dda0b69fa21c
WilliamsParre <williamsparre@gmail.com>
2022-03-13 03:20:58 +0530

    Adds login functionality

--- base/views.py
+++ base/views.py
@@ -1,9 +1,42 @@
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Course
 
+@login_required(login_url='login')
 def home(request):
     courses = Course.objects.all()
     c = {'courses': courses}
     return render(request, 'base/home.html', c)
 
+def login_page(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exists')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password doesnot exist')
+    
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
