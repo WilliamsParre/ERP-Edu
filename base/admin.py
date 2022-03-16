b848180e3b0a9634b19f788ae7bb2081002e1a60
WilliamsParre <williamsparre@gmail.com>
2022-03-16 22:20:34 +0530

    Adds Students to Admin

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course
+from .models import Course, Student
+
+admin.site.site_header = 'ERP Admin Panel'
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('u_id','first_name','last_name','gender','email', 'mobile')
+
 
 admin.site.register(Course)
+admin.site.register(Student, StudentAdmin)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from turtle import mode, update
 from django.db import models
 
@@ -13,3 +14,30 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    u_id = models.BigIntegerField()
+    email = models.EmailField()
+    gender = models.CharField(max_length=10, choices=[('Male','Male'), ('Female', 'Female'), ('Others', 'Others')])
+    profile_pic = models.ImageField(upload_to='base/templates/')
+    mobile = models.BigIntegerField()
+    blood = models.CharField(max_length=5)
+    parent_name = models.CharField(max_length=200)
+    parent_mobile = models.BigIntegerField()
+    relation = models.CharField(max_length=200)
+    cast = models.CharField(max_length=20)
+    birth_place = models.CharField(max_length=20)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    nationality = models.CharField(max_length=40)
+    admission_date = models.DateField()
+
+
+
+
+
+    
+
