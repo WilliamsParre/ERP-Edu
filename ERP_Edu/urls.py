15723974575da177a147a90c6cd1a6a0326cedb9
WilliamsParre <williamsparre@gmail.com>
2022-04-02 00:18:48 +0530

    Modifies Admin path

--- ERP_Edu/urls.py
+++ ERP_Edu/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('user/login/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     path('', include('home.urls')),
     path('user/', include('base.urls'))
